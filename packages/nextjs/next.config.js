97c1dc6360559023ec23e2530a060c11f458831f
shigerubt <z648s.7bt@gmail.com>
2026-03-10 13:26:39 -0600

    feat: Add Next.js configuration for static export and Firebase hosting setup.

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -3,6 +3,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   typescript: {
     ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
